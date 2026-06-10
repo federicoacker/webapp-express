05e72bbea6f4c97d14baa756efc2f0a41026e26c
federicoacker <acker.federico@gmail.com>
2026-06-10 12:47:10 +0200

    hotfix, cambiato nome ai parametri nel create connection di db.js

--- data/db.js
+++ data/db.js
@@ -1,11 +1,11 @@
 import { createConnection } from 'mysql2/promise';
 
 const connection = await createConnection({
-    host: process.env.DB_HOSTNAME,
-    port: process.env.DB_PORT,
-    user: process.env.DB_USERNAME,
-    password: process.env.DB_PASSWORD,
-    database: process.env.DB_DATABASE
+    host: process.env.DATABASE_URL,
+    port: process.env.DATABASE_PORT,
+    user: process.env.DATABASE_USER,
+    password: process.env.DATABASE_PASSWORD,
+    database: process.env.DATABASE_DB
 });
 
 export default connection;
