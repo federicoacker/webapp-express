c10bdfa1d5dee204efa6be4a350719f62c5e07c1
SimonePalmisciano <simonepalmisciano11@gmail.com>
2026-06-10 13:41:47 +0200

    creata la show

--- controllers/reviews.js
+++ controllers/reviews.js
@@ -1,3 +1,5 @@
+import connection from "../data/db";
+
 const reviewController = {
     index,
     show,
@@ -6,24 +8,62 @@ const reviewController = {
     destroy
 }
 
-function index (request, response) {
+function index(request, response) {
 
 }
 
-function show (request, response) {
+async function show(request, response) {
+    const slug = request.slug
+    const results = await reviewSelectAllForProduct(slug);
+
+    if (!results) {
+        return response.status(500)
+            .json({
+                error: "",
+                result: null
+            })
+    }
+
+    try {
+        const [productRows] = await connection.execute(
+            'SELECT * FROM products WHERE slug = ?',
+            [slug]
+        );
+
+        if (productRows.length === 0) {
+            response.status(404)
+                .json({
+                    error: 'prodotto non trovato',
+                    result: null
+                });
+        }
+
+        response.json({
+            error: null,
+            result: productRows[0]
+        })
+
+    } catch (error) {
+        console.error(error);
+        response.status(500)
+            .json({
+                errore: 'errore del server',
+                risultato: null
+            });
+    }
 
 }
 
-function store (request, response) {
+function store(request, response) {
 
 }
 
-function modify (request, response) {
-    
+function modify(request, response) {
+
 }
 
-function destroy (request, response){
-    
+function destroy(request, response) {
+
 }
 
 export default reviewController;
