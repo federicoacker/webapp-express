625b698c2a496c38796681337e4e96e5a7adeae4
federicoacker <acker.federico@gmail.com>
2026-06-10 12:44:44 +0200

    Creato controllers/reviews.js e routers/reviews.js

---
+++ controllers/reviews.js
@@ -0,0 +1,29 @@
+const reviewController = {
+    index,
+    show,
+    store,
+    modify,
+    destroy
+}
+
+function index (request, response) {
+
+}
+
+function show (request, response) {
+
+}
+
+function store (request, response) {
+
+}
+
+function modify (request, response) {
+    
+}
+
+function destroy (request, response){
+    
+}
+
+export default reviewController;
