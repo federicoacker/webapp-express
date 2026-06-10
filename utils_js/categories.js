9b9cb1da7b2465d02b594909f1ce3c3365b3d671
Raffaele De Pasca <depascaraffaele@gmail.com>
2026-06-10 18:27:19 +0200

    products e categories

---
+++ utils_js/categories.js
@@ -0,0 +1,52 @@
+const categories = [
+  {
+    slug: "burgers",
+    label: "Burgers",
+    description: "Selezione di burger con polpette di carne, verdure fresche e salse.",
+    image: "",
+  },
+  {
+    slug: "bites",
+    label: "Bites",
+    description: "Bocconcini e nuggets di carne, ideali come spuntino o da condividere.",
+    image: "",
+  },
+  {
+    slug: "wraps",
+    label: "Wraps",
+    description: "Piadine farcite con ingredienti freschi, carne e salse a scelta.",
+    image: "",
+  },
+  {
+    slug: "sides",
+    label: "Sides",
+    description: "Contorni per accompagnare il pasto: patatine, anelli di cipolla e altro.",
+    image: "",
+  },
+  {
+    slug: "sauces",
+    label: "Sauces",
+    description: "Salse artigianali disponibili in vari gusti, dalla delicata alla piccante.",
+    image: "",
+  },
+  {
+    slug: "drinks",
+    label: "Drinks",
+    description: "Bevande fredde e calde: bibite, succhi, milkshake e molto altro.",
+    image: "",
+  },
+  {
+    slug: "desserts",
+    label: "Desserts",
+    description: "Dolci e sfizi zuccherati per concludere il pasto.",
+    image: "",
+  },
+  {
+    slug: "menus",
+    label: "Menus",
+    description: "Combinazioni complete con burger, contorno e bevanda a prezzo fisso.",
+    image: "",
+  },
+];
+
+export default categories;
