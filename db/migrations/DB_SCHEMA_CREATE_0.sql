f4c05b4caab29db05fe05bcfc14fd8afa76f2692
federicoacker <acker.federico@gmail.com>
2026-06-10 12:19:03 +0200

    Aggiornato il file sql che dovrebbe essere giusto ora

--- db/migrations/DB_SCHEMA_CREATE_0.sql
+++ db/migrations/DB_SCHEMA_CREATE_0.sql
@@ -10,6 +10,7 @@ ALTER TABLE
     `products` ADD UNIQUE `products_name_unique`(`name`);
 ALTER TABLE
     `products` ADD UNIQUE `products_slug_unique`(`slug`);
+    
 CREATE TABLE `categories`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `slug` VARCHAR(255) NOT NULL,
@@ -17,10 +18,12 @@ CREATE TABLE `categories`(
     `description` TEXT NOT NULL,
     `image` VARCHAR(255) NOT NULL
 );
+
 ALTER TABLE
     `categories` ADD UNIQUE `categories_slug_unique`(`slug`);
 ALTER TABLE
     `categories` ADD UNIQUE `categories_label_unique`(`label`);
+    
 CREATE TABLE `reviews`(
     `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY,
     `product_id` BIGINT UNSIGNED NOT NULL,
@@ -31,24 +34,26 @@ CREATE TABLE `reviews`(
     `likes` BIGINT NOT NULL DEFAULT 0,
     `date` DATETIME NOT NULL
 );
+
 ALTER TABLE
     `reviews` ADD INDEX `reviews_product_id_index`(`product_id`);
 ALTER TABLE
     `reviews` ADD UNIQUE `reviews_slug_unique`(`slug`);
+    
 CREATE TABLE `category_product`(
     `product_id` BIGINT UNSIGNED NOT NULL,
     `category_id` BIGINT UNSIGNED NOT NULL,
     PRIMARY KEY(`product_id`, `category_id`)
 );
-ALTER TABLE
-    `category_product` ADD CONSTRAINT `category_product_product_id_foreign` FOREIGN KEY(`product_id`) REFERENCES `products`(`id`);
-ALTER TABLE
-    `reviews` ADD CONSTRAINT `reviews_product_id_foreign` FOREIGN KEY(`product_id`) REFERENCES `products`(`id`);
-ALTER TABLE
-    `category_product` ADD CONSTRAINT `category_product_category_id_foreign` FOREIGN KEY(`category_id`) REFERENCES `categories`(`id`);
 
 ALTER TABLE
-	`products` ADD COLUMN `created_at` TIMESTAMP NOT NULL;
+    `category_product` ADD CONSTRAINT `category_product_product_id_foreign` FOREIGN KEY(`product_id`) REFERENCES `products`(`id`) ON DELETE CASCADE;
+ALTER TABLE
+    `reviews` ADD CONSTRAINT `reviews_product_id_foreign` FOREIGN KEY(`product_id`) REFERENCES `products`(`id`) ON DELETE SET NULL;
+ALTER TABLE
+    `category_product` ADD CONSTRAINT `category_product_category_id_foreign` FOREIGN KEY(`category_id`) REFERENCES `categories`(`id`) ON DELETE CASCADE;
     
 ALTER TABLE
-	`products` ADD COLUMN `updated_at` TIMESTAMP NOT NULL;
+	`products` ADD COLUMN `created_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP;
+ALTER TABLE
+	`products` ADD COLUMN `updated_at` TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP;
